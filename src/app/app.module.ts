a05dd4503a64b234cdb44389d803c7f3157c6729
Ray Everton <ray.evaf@gmail.com>
2019-06-17 20:06:11 -0300

    adding navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NavbarCompenent } from "./navbar/navbar.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarCompenent
   ],
   imports: [
     BrowserModule,
